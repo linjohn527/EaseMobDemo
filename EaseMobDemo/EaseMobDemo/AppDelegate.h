f83a9c26ecb7e779a673edb39882fdcf2eb94b39
linjohn527 <linjohn@linjohns-mac.local>
2016-03-26 19:59:13 +0800

    初始化项目

---
+++ EaseMobDemo/EaseMobDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EaseMobDemo
+//
+//  Created by linjohn on 3/26/16.
+//  Copyright © 2016 linjohn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EaseMobDemo/EaseMobDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EaseMobDemo
+//
+//  Created by linjohn on 3/26/16.
+//  Copyright © 2016 linjohn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
